3d53e133823b10e26f9a6b40fb6fef907aada05b
valad47 <valad.racz@gmail.com>
2025-04-22 21:23:18 +0300

    -

--- main.c
+++ main.c
@@ -133,3 +133,72 @@ int vlm_RequestPost(lua_State *L) {
 
     return 2;
 }
+
+int vlm_RequestPostForm(lua_State *L) {
+    if(gc_cycle == 0) {
+        lua_gc(L, LUA_GCSTOP, 0);
+    } else if (gc_cycle == 2) {
+        //lua_gc(L, LUA_GCCOLLECT, 0);
+        gc_cycle = 0;
+    }
+    gc_cycle++;
+    struct MemoryStruct body = {0}, header = {0};
+    body.memory = malloc(1);
+    header.memory = malloc(1);
+
+    CURL *handle = curl_easy_init();
+    curl_easy_setopt(handle, CURLOPT_URL, luaL_checkstring(L, 1));
+    curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, CURLFOLLOW_ALL);
+    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+    curl_easy_setopt(handle, CURLOPT_WRITEDATA, &body);
+    curl_easy_setopt(handle, CURLOPT_HEADERFUNCTION, WriteMemoryCallback);
+    curl_easy_setopt(handle, CURLOPT_HEADERDATA, &header);
+
+    curl_mime *mime = curl_mime_init(handle);
+    if(lua_istable(L, 2)) {
+        lua_pushnil(L);
+        while(lua_next(L, 2) != 0) {
+            curl_mimepart *part = curl_mime_addpart(mime);
+
+            size_t size;
+            const char *data = luaL_checklstring(L, -1, &size);
+            curl_mime_data(part, data, size);
+            curl_mime_name(part, luaL_checkstring(L, -2));
+            lua_pop(L, 1);
+        }
+    }
+
+    if(lua_istable(L, 3)) {
+        lua_pushnil(L);
+        while(lua_next(L, 3) != 0) {
+            curl_mimepart *part = curl_mime_addpart(mime);
+
+            const char *data = luaL_checkstring(L, -1);
+            curl_mime_filedata(part, data);
+            curl_mime_name(part, luaL_checkstring(L, -2));
+            lua_pop(L, 1);
+        }
+    }
+
+
+    curl_easy_setopt(handle, CURLOPT_MIMEPOST, mime);
+
+    CURLcode res;
+    if((res = curl_easy_perform(handle)) != CURLE_OK) {
+        curl_easy_cleanup(handle);
+        curl_mime_free(mime);
+        free(body.memory);
+        free(header.memory);
+        luaL_error(L, "Failed to perform POST request: %s\n", curl_easy_strerror(res));
+    }
+
+    lua_pushstring(L, header.memory);
+    lua_pushstring(L, body.memory);
+
+    curl_easy_cleanup(handle);
+    curl_mime_free(mime);
+    free(body.memory);
+    free(header.memory);
+
+    return 2;
+}
